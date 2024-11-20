f58e72dae3868282ceb2034a90656c7dce9a3246
Udbasili <33573587+udabasili@users.noreply.github.com>
2024-11-20 00:37:28 -0400

    Added Routing system skeleton and middleware

---
+++ server/src/api/routes/user.ts
@@ -0,0 +1,13 @@
+import {Router} from "express";
+import confirmAuthentication from "../middlewares/confirmAuthentication";
+import setCurrentUser from "@/api/middlewares/setCurrentUser";
+
+
+export default (app: Router) =>{
+    const route = Router();
+
+    app.use('/user/:userId', confirmAuthentication, setCurrentUser , route)
+    route.get('', (req, res) => {
+        res.status(200).json({ message: req.currentUser})
+    })
+}
