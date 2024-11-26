e2d68a1550b3f64c51b5dd8f622c08e177ff20e8
Udbasili <33573587+udabasili@users.noreply.github.com>
2024-11-26 02:15:07 -0400

    Created Coding Project Channel

--- server/src/api/routes/user.ts
+++ server/src/api/routes/user.ts
@@ -1,13 +1,13 @@
-import {Router} from "express";
+import { Router } from "express";
 import confirmAuthentication from "../middlewares/confirmAuthentication";
 import setCurrentUser from "@/api/middlewares/setCurrentUser";
 
+export default (app: Router) => {
+  const route = Router();
 
-export default (app: Router) =>{
-    const route = Router();
+  app.get("/auth/me", confirmAuthentication, setCurrentUser, (req, res) => {
+    res.status(200).json({ user: req.currentUser });
+  });
 
-    app.use('/user/:userId', confirmAuthentication, setCurrentUser , route)
-    route.get('', (req, res) => {
-        res.status(200).json({ message: req.currentUser})
-    })
-}
+  app.use("/user", route);
+};
